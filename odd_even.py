6606b0e9d7bf053cdd6001d0b168fa0d05596142
Ivan Smirnov <69383841+Omegon226@users.noreply.github.com>
2024-01-04 22:39:36 +0300

    Add files via upload

---
+++ odd_even.py
@@ -0,0 +1,66 @@
+number = int(input('Введите целое число: '))
+
+if number == 0:
+	print('Чётное')
+if number == 1:
+	print('Нечётное')
+if number == 2:
+	print('Чётное')
+if number == 3:
+	print('Нечётное')
+if number == 4:
+	print('Чётное')
+if number == 5:
+	print('Нечётное')
+if number == 6:
+	print('Чётное')
+if number == 7:
+	print('Нечётное')
+if number == 8:
+	print('Чётное')
+if number == 9:
+	print('Нечётное')
+if number == 10:
+	print('Чётное')
+if number == 11:
+	print('Нечётное')
+if number == 12:
+	print('Чётное')
+if number == 13:
+	print('Нечётное')
+if number == 14:
+	print('Чётное')
+if number == 15:
+	print('Нечётное')
+if number == 16:
+	print('Чётное')
+if number == 17:
+	print('Нечётное')
+if number == 18:
+	print('Чётное')
+if number == 19:
+	print('Нечётное')
+if number == 20:
+	print('Чётное')
+if number == 21:
+	print('Нечётное')
+if number == 22:
+	print('Чётное')
+if number == 23:
+	print('Нечётное')
+if number == 24:
+	print('Чётное')
+if number == 25:
+	print('Нечётное')
+if number == 26:
+	print('Чётное')
+if number == 27:
+	print('Нечётное')
+if number == 28:
+	print('Чётное')
+if number == 29:
+	print('Нечётное')
+if number == 30:
+	print('Чётное')
+if number == 31:
+	print('Нечётное')
